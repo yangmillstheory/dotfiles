a60322837bacaac1bf3549f9ce7117380f7b7889
Victor Alvarez <victoralvarez@google.com>
2023-03-28 20:47:57 +0200

    Fold based on nvim-treesitter AST by default

--- nvim/.config/nvim/after/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/after/plugin/nvim-treesitter.lua
@@ -3,3 +3,6 @@ require('nvim-treesitter.configs').setup({
   ensure_installed = { 'cpp', 'lua', 'vim', 'go', 'python'},
   highlight = { enable = true }
 })
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -3,7 +3,7 @@ local api = vim.api
 local vimrc = api.nvim_create_augroup('vimrc', { clear = true })
 api.nvim_create_autocmd({ 'BufRead' }, {
   pattern = 'vimrc',
-  callback = function(t)
+  callback = function()
     vim.opt_local.foldmethod = 'marker'
     vim.opt_local.foldlevelstart = 0
   end
