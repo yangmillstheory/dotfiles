83b2c69f7d70ad5b9053ccc38e872245ba20b747
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-01-24 15:47:50 +0100

    Fix regex; yank into clipboard

--- nvim/.config/nvim/lua/jp.lua
+++ nvim/.config/nvim/lua/jp.lua
@@ -1,10 +1,12 @@
 vim.api.nvim_create_user_command('YomitanRmNums',
+-- TODO: <span> and other HTML tags can mess this up?
   function(_)
     -- Strip out tags like (\d+).
-    vim.cmd([[%s/\v\(\d+\)\s//g]])
+    vim.cmd([[silent! %s/\v\(\d+\)\s//g]])
     -- Strip out tags like (1, \w+).
-    vim.cmd([[%s/\v\(\d+,\s(.+)\)/(\1)/g]])
-    -- TODO: <span> tags can mess this up?
+    vim.cmd([[silent! %s/\v\(\d+,\s([^)]+)\)/(\1)/g]])
+    -- Yank into the system clipboard.
+    vim.cmd.normal('"*yy')
   end
 , {
   desc = 'Remove redundant numerical meaning tags like (1), (2), from mined Yomitan HTML definitions.'
@@ -19,6 +21,8 @@ vim.api.nvim_create_user_command('YomitanCleanKanjiDef',
     vim.cmd.normal('3dF<x')
     -- Replace interior </li><li> (etc) tags with |.
     vim.cmd([[%s/\v(\<\/?[oli]{2}\>){2}/ | /g]])
+    -- Yank into the system clipboard.
+    vim.cmd.normal('"*yy')
   end
 , {
   desc = 'Make Yomitan-mined Kanji not be HTML lists, but plain text separated by |.'
