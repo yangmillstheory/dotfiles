d92614f4930e5b987a3a7c6ef022e34e7330fb5c
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-02-07 12:13:35 +0100

    indent-blankline is broken on HTML

--- nvim/.config/nvim/after/plugin/indent-blankline.nvim.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.nvim.lua
@@ -1,4 +1,18 @@
 vim.opt.list = true
 vim.g.indent_blankline_filetype = { "cpp", "python", "lua", }
 
-require("ibl").setup {}
+require("ibl").setup {
+  exclude = {
+    filetypes = {
+      -- breaks tree-sitter highlighting
+      'html',
+      -- defaults
+      'lspinfo',
+      'packer',
+      'checkhealth',
+      'help',
+      'man',
+      'gitcommit',
+    }
+  }
+}
