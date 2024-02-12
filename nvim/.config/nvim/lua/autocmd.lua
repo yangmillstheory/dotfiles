471ae3eab0f405f32eda39d37805179bb047b43e
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-02-12 19:11:33 +0100

    Some code to toggle markdown checklist items

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = {"*"},
+  desc = "Remove lines with only whitespace before writing buffer.",
   callback = function(_)
     local view = vim.fn.winsaveview()
     vim.cmd([[%s/\s\+$//e]])
