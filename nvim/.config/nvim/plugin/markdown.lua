585d4475243cf6f113a7289aca5b24911fb840a3
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-02-13 09:45:46 +0100

    Use early return

--- nvim/.config/nvim/plugin/markdown.lua
+++ nvim/.config/nvim/plugin/markdown.lua
@@ -8,12 +8,13 @@ local function ToggleTodo()
   local old_todo = vim.api.nvim_get_current_line()
   local new_todo = nil
   local status = string.match(old_todo, "^%s*%*%s%[(.)%]%s.+$")
+  if status ~= ' ' and status ~= 'x' then
+    return
+  end
   if status == ' ' then
     new_todo = string.gsub(old_todo, "%b[]", '[x]', 1)
-  elseif status == 'x' then
-    new_todo = string.gsub(old_todo, "%b[]", '[ ]', 1)
   else
-    return
+    new_todo = string.gsub(old_todo, "%b[]", '[ ]', 1)
   end
   local row, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_lines(0, row - 1, row, true, {new_todo})
