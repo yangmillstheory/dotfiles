78a2c58c8447bd35f25dda6b26bf9728a64d4936
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-02-14 22:11:32 +0100

    clean

--- nvim/.config/nvim/lua/jp.lua
+++ nvim/.config/nvim/lua/jp.lua
@@ -38,12 +38,11 @@ vim.api.nvim_create_user_command('YomitanCleanKanji',
   desc = 'Make Yomitan-mined Kanji not be HTML lists, but plain text separated by |.'
 })
 
-vim.api.nvim_create_user_command('YomitanSingleTerm',
+vim.api.nvim_create_user_command('YomitanOneDefinitionTerm',
   function(_)
     -- These have a predictale structure: ... <li><i>(...)</i>(definition)</li> ...
     -- Remove everything up to the first <i>, skip to the </li>, delete to the end.
-    vim.cmd.normal('3dt<')
-    vim.cmd.normal('2f<d$')
+    vim.cmd.normal('3dt<2f<d$')
   end
 , {
   desc = 'Clean Yomitan-mined terms with a single non-forms definition.'
