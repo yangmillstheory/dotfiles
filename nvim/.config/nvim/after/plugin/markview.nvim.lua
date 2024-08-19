a7c825a74c8a5a3865fd7b6b5871cdfa894a6aa8
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-08-19 17:38:42 +0900

    Don't use cursorcolumn for markdown files.
    See https://github.com/OXY2DEV/markview.nvim/issues/112.

--- nvim/.config/nvim/after/plugin/markview.nvim.lua
+++ nvim/.config/nvim/after/plugin/markview.nvim.lua
@@ -7,6 +7,15 @@ markview.setup({
     enable = true,
     shift_width = 2,
     indent_size = 2,
+  },
+
+  modes = { "n", "no", "c" },
+  hybrid_modes = { "n" },
+  callbacks = {
+    on_enable = function (_, win)
+      vim.wo[win].conceallevel = 2;
+      vim.wo[win].concealcursor = "c";
+    end
   }
 });
 
