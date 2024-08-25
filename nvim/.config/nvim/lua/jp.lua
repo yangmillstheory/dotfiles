e60409f1ba944f43822fb052c42cacfb139f3743
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2024-08-25 21:45:43 +0900

    Add LookupTerm; refactor; allow for Hiragana and Katakana too

--- nvim/.config/nvim/lua/jp.lua
+++ nvim/.config/nvim/lua/jp.lua
@@ -128,15 +128,47 @@ vim.api.nvim_create_user_command('YomitanPrepareWords',
   desc = 'Prepare words with Furigana for a Kanji note.'
 })
 
+local function _lookup(url_templates, query)
+  print('d')
+  for _, url_template in ipairs(url_templates) do
+    local url = { string.format(url_template, query) }
+    vim.loop.spawn(
+      "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+      { args = url },
+      function(code, signal)
+        if code ~= 0 then
+          print(string.format('Opening URL %s exited with code: %d and signal %d',
+            url, code, signal))
+        end
+      end
+    )
+  end
+end
+
+local function _is_jp_char(codepoint)
+  if type(codepoint) ~= "number" then
+    return false
+  end
+  return (codepoint >= 0x4E00 and codepoint <= 0x9FFF) or    -- Kanji
+    (codepoint >= 0x3400 and codepoint <= 0x4DBF) or    -- Kanji Extension
+    (codepoint >= 0x20000 and codepoint <= 0x2A6DF) or  -- Kanji Supplement
+    (codepoint >= 0x2A700 and codepoint <= 0x2B73F) or  -- Kanji Supplement
+    (codepoint >= 0x2B740 and codepoint <= 0x2B81F) or  -- Kanji Supplement
+    (codepoint >= 0x2B820 and codepoint <= 0x2CEAF) or  -- Kanji Supplement
+    (codepoint >= 0x2CEB0 and codepoint <= 0x2EBEF) or  -- Kanji Supplement
+    (codepoint >= 0x3040 and codepoint <= 0x309F) or    -- Hiragana
+    (codepoint >= 0x30A0 and codepoint <= 0x30FF)       -- Katakana
+end
+
+
 vim.api.nvim_create_user_command('LookupKanji',
   function (opts)
     local kanji = opts.args
-    local cp = utf8.codepoint(kanji)
-    if (cp < 0x4E00 or cp > 0x9FFF) and (cp < 0x3400 or cp > 0x4DBF) then
+    if not _is_jp_char(utf8.codepoint(kanji)) then
       print(string.format('%s is not a valid Japanese Kanji.', kanji))
       return
     end
-    local url_templates = {
+    _lookup({
       "https://www.immersionkit.com/dictionary?keyword=%s&sort=shortness&category=drama#",
       "https://kanji.koohii.com/study/kanji/%s",
       "https://jisho.org/search/*%s*",
@@ -144,29 +176,46 @@ vim.api.nvim_create_user_command('LookupKanji',
       "https://tatoeba.org/en/sentences/search?from=jpn&has_audio=yes&query=%s&sort=relevance&to=eng&trans_to=eng",
       -- No audio required.
       "https://tatoeba.org/en/sentences/search?from=jpn&query=%s&sort=relevance&to=eng&trans_to=eng",
-    }
-    for _, url_template in ipairs(url_templates) do
-      local url = { string.format(url_template, kanji) }
-      vim.loop.spawn(
-        "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
-        { args = url },
-        function(code, signal)
-          if code == 0 then
-            return
-          end
-          print(string.format('Opening URL %s exited with code: %d and signal %d',
-            url, code, signal))
-        end
-      )
-    end
+    }, kanji)
   end,
   {
-    desc = 'Open relevant Kanji webpages to start Anki card creation process.',
+    desc = 'Open relevant webpages to start Anki Kanji card creation process.',
     nargs = 1,
   }
 )
 
-keymap('n', '<leader>kk', ':LookupKanji <c-r><c-w><cr>')
+vim.api.nvim_create_user_command('LookupTerm',
+  function ()
+    local term = vim.fn.expand('<cWORD>')
+    for _, cp in utf8.codes(term) do
+      -- NB: Using https://github.com/uga-rosa/utf8.nvim/blob/954cbbadabe5cd19f202e918fec191d64eea7766/lua/utf8.lua#L158
+      -- Don't be misled by https://www.lua.org/manual/5.4/manual.html#pdf-utf8.codes. This API doesn't exist in the current
+      -- Lua runtime, which is Lua 5.1. `cp` is actually a string, not an integer, despite any LSP warnings.
+      if not _is_jp_char(utf8.codepoint(cp)) then
+        print(
+          string.format('%s is not a valid Japanese Kanji because of %s.',
+          term, cp)
+        )
+        return
+      end
+    end
+    print('d')
+    _lookup({
+      "https://www.immersionkit.com/dictionary?keyword=%s&sort=shortness&category=drama#",
+      -- Ask for audio on the off-chance that it exists.
+      "https://tatoeba.org/en/sentences/search?from=jpn&has_audio=yes&query=%s&sort=relevance&to=eng&trans_to=eng",
+      -- No audio required.
+      "https://tatoeba.org/en/sentences/search?from=jpn&query=%s&sort=relevance&to=eng&trans_to=eng",
+      }, term)
+  end,
+  {
+    desc = 'Open relevant webpages to start Anki Term card creation process.',
+    nargs = 0,
+  }
+)
+
+keymap('n', '<leader>lk', ':LookupKanji <c-r><c-w><cr>')
+keymap('n', '<leader>lt', ':LookupTerm<cr>')
 keymap('n', '<leader>yd', ':YomitanCleanDefinition<cr>')
 keymap('n', '<leader>yp', ':YomitanPrepareWords<cr>')
 keymap('n', '<leader>yk', ':YomitanCleanKanji<cr>')
