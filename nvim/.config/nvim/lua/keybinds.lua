524ca6bc04e411f0a63b9b3971edd959d5b552e9
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2025-01-01 18:50:00 +0900

    Default to regex mode

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -33,8 +33,8 @@ keymap('v', '<A-z>', 'za')
 
 -- search & replace
 keymap('n', '<leader><esc>', ':noh<cr>', { silent = true })
-keymap('n', '<A-r>', ':%s///g<left><left><left>')
-keymap('v', '<A-r>', ':s///g<left><left><left>')
+keymap('n', '<A-r>', ':%s/\\v//g<left><left><left>')
+keymap('v', '<A-r>', ':s/\\v//g<left><left><left>')
 
 -- writing, quitting, opening
 keymap('n', '<A-w>', ':w<cr>')
