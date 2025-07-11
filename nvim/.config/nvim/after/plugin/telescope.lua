f1e0a01a5f7cb7b1919e3e6e445d03720f9e3af1
Victor Alvarez <victor.alvarez@woven-planet.global>
2025-07-11 09:50:34 +0900

    Telescope live_grep_args

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
+local lga_actions = require("telescope-live-grep-args.actions")
 local utils = require('utils')
 
 require('telescope').setup {
@@ -20,13 +22,33 @@ require('telescope').setup {
     live_grep = {
       theme = 'ivy'
     },
+  },
+  extensions = {
+    live_grep_args = {
+      auto_quoting = true, -- enable/disable auto-quoting
+      -- define mappings, e.g.
+      mappings = { -- extend mappings
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          -- freeze the current list and start a fuzzy search in the frozen list
+          ["<C-space>"] = lga_actions.to_fuzzy_refine,
+        },
+      },
+      -- ... also accepts theme settings, for example:
+      -- theme = "dropdown", -- use dropdown theme
+      -- theme = { }, -- use own theme spec
+      -- layout_config = { mirror=true }, -- mirror preview pane
+    }
   }
 }
+
+telescope.load_extension("live_grep_args")
 
 utils.keymap('n', '<c-t>', function()
   builtin.find_files({ hidden = true })
 end, { desc = 'Telescope find files' })
-utils.keymap('n', '<c-f>', builtin.live_grep, { desc = 'Telescope live grep'})
+utils.keymap('n', '<c-f>', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep'})
 utils.keymap('n', '<c-b>', builtin.buffers, { desc = 'Telescope buffers'})
 utils.keymap('n', '<c-r>', builtin.command_history, { desc = 'Telescope command history'})
 utils.keymap('n', '<c-s>', builtin.search_history, { desc = 'Telescope search history'})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -58,7 +58,15 @@ require("lazy").setup({
   },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+          "nvim-telescope/telescope-live-grep-args.nvim" ,
+          -- This will not install any breaking changes.
+          -- For major updates, this must be adjusted manually.
+          version = "^1.0.0",
+      },
+    },
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   'rebelot/kanagawa.nvim',
