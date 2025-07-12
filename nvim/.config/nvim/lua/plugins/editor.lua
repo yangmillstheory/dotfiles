0773193b9dac3229b36ad7144bb5edf8c244f702
Victor Alvarez <victor.alvarez@woven-planet.global>
2025-07-12 17:39:16 +0900

    Add back mappings

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -23,11 +23,11 @@ return {
 
       utils.keymap('v', '<A-S-h>', '<Plug>MoveBlockLeft')
       utils.keymap('v', '<A-S-j>', '<Plug>MoveBlockDown')
-      utils.keymap('v', '<A-S-k>', '<Plug>MoveBlockUp')
-      utils.keymap('v', '<A-S-l>', '<Plug>MoveBlockRight')
       utils.keymap('n', '<A-S-h>', '<Plug>MoveCharLeft')
       utils.keymap('n', '<A-S-j>', '<Plug>MoveLineDown')
       utils.keymap('n', '<A-S-k>', '<Plug>MoveLineUp')
+      utils.keymap('v', '<A-S-k>', '<Plug>MoveBlockUp')
+      utils.keymap('v', '<A-S-l>', '<Plug>MoveBlockRight')
       utils.keymap('n', '<A-S-l>', '<Plug>MoveCharRight')
     end
   },
@@ -101,6 +101,33 @@ return {
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   { 'windwp/nvim-autopairs', config = true },
-  'yangmillstheory/vim-snipe',
+  {
+    'yangmillstheory/vim-snipe',
+    config = function()
+      local utils = require('utils')
+      utils.keymap('', '<leader><leader>F', '<Plug>(snipe-F)')
+      utils.keymap('', '<leader><leader>f', '<Plug>(snipe-f)')
+      utils.keymap('', '<leader><leader>T', '<Plug>(snipe-T)')
+      utils.keymap('', '<leader><leader>t', '<Plug>(snipe-t)')
+      utils.keymap('', '<leader><leader>w', '<Plug>(snipe-w)')
+      utils.keymap('', '<leader><leader>W', '<Plug>(snipe-W)')
+      utils.keymap('', '<leader><leader>e', '<Plug>(snipe-e)')
+      utils.keymap('', '<leader><leader>E', '<Plug>(snipe-E)')
+      utils.keymap('', '<leader><leader>b', '<Plug>(snipe-b)')
+      utils.keymap('', '<leader><leader>B', '<Plug>(snipe-B)')
+      utils.keymap('', '<leader><leader>ge', '<Plug>(snipe-ge)')
+      utils.keymap('', '<leader><leader>gE', '<Plug>(snipe-gE)')
+      utils.keymap('n', '<leader><leader>]', '<Plug>(snipe-f-xp)')
+      utils.keymap('n', '<leader><leader>[', '<Plug>(snipe-F-xp)')
+      utils.keymap('n', '<leader><leader>x', '<Plug>(snipe-f-x)')
+      utils.keymap('n', '<leader><leader>X', '<Plug>(snipe-F-x)')
+      utils.keymap('n', '<leader><leader>r', '<Plug>(snipe-f-r)')
+      utils.keymap('n', '<leader><leader>R', '<Plug>(snipe-F-r)')
+      utils.keymap('n', '<leader><leader>i', '<Plug>(snipe-f-i)')
+      utils.keymap('n', '<leader><leader>I', '<Plug>(snipe-F-i)')
+      utils.keymap('n', '<leader><leader>a', '<Plug>(snipe-f-a)')
+      utils.keymap('n', '<leader><leader>A', '<Plug>(snipe-F-a)')
+    end
+  },
   'tpope/vim-obsession',
 }
