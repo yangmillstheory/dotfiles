54c9789576c9beac6cecbae9d6d4b0ce31f9f2fc
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2025-07-26 21:35:23 +0900

    cleaner scroll

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -129,11 +129,7 @@ return {
   },
   {
     "sphamba/smear-cursor.nvim",
-    opts = {
-      stiffness = 0.5,
-      trailing_stiffness = 0.49,
-      never_draw_over_target = false,
-    },
+    opts = {},
   },
   {
     "karb94/neoscroll.nvim",
