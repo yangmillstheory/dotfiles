824ffef014cff240a49fd8537af1edd32679d97b
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2025-08-02 15:15:46 +0900

    rm comment

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -134,7 +134,6 @@ return {
     config = function()
       require("noice").setup({
         lsp = {
-          -- overr
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
