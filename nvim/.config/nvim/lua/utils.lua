a6a8eedec60d2c86e5d00b60478f49aa45c2dfa4
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2025-08-22 19:35:57 +0900

    stylua

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -2,11 +2,11 @@ local module = {}
 
 -- Create a keymap with default options.
 function module.keymap(mode, lhs, rhs, options)
-  local opts = { noremap = true, silent = true }
-  if options and type(options) == 'table' then
-    opts = vim.tbl_extend('force', opts, options)
-  end
-  vim.keymap.set(mode, lhs, rhs, opts)
+	local opts = { noremap = true, silent = true }
+	if options and type(options) == "table" then
+		opts = vim.tbl_extend("force", opts, options)
+	end
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 return module
