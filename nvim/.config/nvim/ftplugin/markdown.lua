bb44ebe9784a5e00133d4def6499d756d060e9d8
Victor Alvarez <sapien.reversing.entropy@gmail.com>
2025-08-24 12:51:01 +0900

    feat(markdown): improve checkbox toggle keymap
    Refined the markdown checkbox toggle function to reliably switch between
    checked and unchecked states for checklist items. The new implementation
    works on lines starting with '* [ ]' or similar formats, enhancing
    usability for markdown todo lists.

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,47 +1,17 @@
 local keymap = require("utils").keymap
 
--- TODO: handle different kinds of markdown bullets, like -
-if not table.unpack then
-	table.unpack = unpack
-end
-
--- Using "-" makes it compatible with markview.nvim's rendering.
-local function ToggleDone()
-	local old = vim.api.nvim_get_current_line()
-	local new = nil
-	local status = string.match(old, "^%s*%*%s%[(.)%]%s.+$")
-	if status ~= " " and status ~= "-" then
-		return
+function ToggleDone()
+	local line = vim.api.nvim_get_current_line()
+	local new_line = line:gsub("%[ %]", "[x]", 1)
+	if new_line == line then
+		new_line = line:gsub("%[x%]", "[ ]", 1)
 	end
-	if status == " " then
-		new = string.gsub(old, "%b[]", "[-]", 1)
-	else
-		new = string.gsub(old, "%b[]", "[ ]", 1)
-	end
-	local row, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
-	vim.api.nvim_buf_set_lines(0, row - 1, row, true, { new })
-end
-
-local function NewTodo()
-	vim.cmd.normal("o* [ ]")
-	vim.api.nvim_feedkeys("A ", "n", true)
+	vim.api.nvim_set_current_line(new_line)
 end
 
 keymap("n", "<leader>xt", "", {
 	desc = "Toggle a markdown checklist item state.",
 	callback = ToggleDone,
-})
-
-keymap("n", "<leader>nt", "", {
-	desc = "Create a markdown checklist item.",
-	callback = NewTodo,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.cursorcolumn = false
-	end,
 })
 
 vim.api.nvim_create_user_command("LoadJp", function()
